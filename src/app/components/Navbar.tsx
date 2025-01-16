6cf89617f432f02d715ffdf2f3a05f5b99b62af0
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2025-01-17 01:19:01 +0500

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,25 +1,37 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Navbar() {
-    return (
-        <header>
-            <nav>
-                <ul>
-                    <li className="text-gray-800 font-sans font-bold justify-evenly flex text-3xl space-x-4 bg-orange-600 py-2 w-full px-1">
-
-
-                        <Link href="/logo" className="text-white text-left font-serif font-extrabold bg-orange-600 py-1 px-1 min-full text-4xl hover:text-white hover:underline">Dream Bean Café</Link>
-                        
-                        <Link href="/" className="text-right hover:text-white hover:underline">Home</Link>
-
-                        <Link href="/about" className="text-right hover:text-white hover:underline">About</Link>
-
-                        <Link href="/menu" className="text-right hover:text-white hover:underline">Menu</Link>
-                    
-                        <Link href="/contact" className="text-right hover:text-white hover:underline">Contact Us</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="bg-orange-600 py-2">
+      <nav className="container mx-auto px-4">
+        <ul className="flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0 md:space-x-4">
+          <li className="text-3xl md:text-4xl font-extrabold font-serif text-white">
+            <Link href="/" className="hover:underline">Dream Bean Café</Link>
+          </li>
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+            <li>
+              <Link href="/" className="text-white hover:underline text-lg md:text-xl">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="text-white hover:underline text-lg md:text-xl">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/menu" className="text-white hover:underline text-lg md:text-xl">
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="text-white hover:underline text-lg md:text-xl">
+                Contact Us
+              </Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
 }
